a89930d317cf5c2b253b8b81f7ddc1c735b7611e
Insprill <insprill@gmail.com>
2020-12-07 19:42:28 -0500

    Add Debug class

---
+++ src/main/java/net/insprill/cam/utils/Debug.java
@@ -0,0 +1,56 @@
+package net.insprill.cam.utils;
+
+import com.google.common.base.Charsets;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import net.insprill.cam.CAM;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Debug {
+
+    private static final Debug instance = new Debug();
+
+    public static Debug getInstance() {
+        return instance;
+    }
+
+    public String createDebugLink(String URL) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("CAM: ").append(CAM.getInstance().getDescription().getVersion()).append("\n");
+        builder.append("Server: ").append(Bukkit.getVersion()).append("\n");
+        builder.append("API: ").append(Bukkit.getBukkitVersion()).append("\n");
+        builder.append("Java: ").append(System.getProperty("java.version")).append("\n");
+        builder.append("OS: ").append(System.getProperty("os.name")).append("\n");
+
+        builder.append("Plugins:").append("\n");
+        for (Plugin pl : Bukkit.getPluginManager().getPlugins())
+            builder
+                    .append("    ")
+                    .append(pl.getName())
+                    .append(" - ")
+                    .append(pl.getDescription().getVersion())
+                    .append("\n");
+
+        try {
+            URL url = new URL("https://hastebin.com/documents");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+            http.setDoOutput(true);
+            http.setConnectTimeout(10 * 1000);
+            http.setReadTimeout(10 * 1000);
+            http.getOutputStream().write(builder.toString().getBytes(Charsets.UTF_8));
+            JsonObject object = new Gson().fromJson(new InputStreamReader(http.getInputStream(), Charsets.UTF_8), JsonObject.class);
+            return "https://hastebin.com/" + object.get("key").getAsString() + ".yaml";
+        } catch (IOException exception) {
+            return "&cCould not create debug link! Error: " + exception.getMessage();
+        }
+    }
+
+}
